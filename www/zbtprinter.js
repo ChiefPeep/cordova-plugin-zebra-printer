34811a68d1927259f19ae5f674384b990b69d78d
Jorge Cavazos <jorge@imexsol.com>
2018-01-08 15:30:07 -0600

    Updated README; First commit;

---
+++ www/zbtprinter.js
@@ -0,0 +1,18 @@
+var exec = require('cordova/exec');
+
+exports.printLabel = function(mac, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, 'ZebraBluetoothPrinter', 'printLabel', [mac]);
+};
+
+exports.print = function(mac, str, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, 'ZebraBluetoothPrinter', 'print', [mac, str]);
+};
+
+exports.find = function(successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, 'ZebraBluetoothPrinter', 'find', []);
+};
+
+exports.getStatus = function(mac, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, 'ZebraBluetoothPrinter', 'getStatus', [mac])
+}
+
